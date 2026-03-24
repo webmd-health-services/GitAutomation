3103eab0bb2b35d9308b168ee2881597c835cc6f
Aaron Jensen <ajensen@webmd.net>
2026-03-24 11:54:50 -0700

    Only need license notices on code files that get published.

--- Tests/LicenseNotices.Tests.ps1
+++ Tests/LicenseNotices.Tests.ps1
@@ -12,6 +12,7 @@ Describe 'License Notices' {
     It 'has license notice in all published files' {
         $projectRoot = Join-Path -Path $PSScriptRoot -ChildPath '..' -Resolve
         $licenseFilePath = Join-Path -Path $projectRoot -ChildPath 'LICENSE' -Resolve
+        $moduleRoot = Join-Path -Path $projectRoot -ChildPath 'GitAutomation' -Resolve
 
         $noticeLines =
             Get-Content -Path $licenseFilePath -Tail 11 |
@@ -29,40 +30,21 @@ Describe 'License Notices' {
         $licenseNoticeRegex | Write-Debug
 
         $pathsToIgnore = @(
-            'GitAutomation/bin/**',
-            'GitAutomation/en-us/**',
-            'GitAutomation/packages/**',
-            'Source/packages/**',
-            'Source/Git.Automation/bin/**',
-            'Source/Git.Automation/obj/**',
-            '.github/**',
-            '.output/**',
-            '.vscode/**',
-            'packages/**',
-            'PSModules/**',
-            '**/*.dll',
-            '**/*.dll.config',
-            '**/*.pdb',
-            '**/*.md',
-            '**/*.sln',
-            '**/*.csproj',
-            '**/packages.config',
-            '.gitignore',
-            'build.ps1',
-            'NOTICE',
-            '*.json',
-            'whiskey.yml',
-            'GitAutomation/NOTICE',
-            'GitAutomation/LICENSE',
-            '.dotnet/**',
-            'GitAutomation/Functions/**',
-            '**/appveyor.yml',
-           'GitAutomation/gitconfig',
-           '**/*.user'
+            '**\*.dll',
+            '**\*.so',
+            '**\*.dylib',
+            '**\*.json',
+            '**\*.pdb',
+            '**\bin\**\*.config',
+            '**\*.help.txt',
+            'Functions\*.ps1',
+            '*.md',
+            'gitconfig',
+            'NOTICE'
         )
 
         [object[]]$filesMissingLicense =
-            Find-GlobFile -Path $projectRoot -Exclude $pathsToIgnore |
+            Find-GlobFile -Path $moduleRoot -Exclude $pathsToIgnore |
             ForEach-Object {
                 $fileInfo = $_
                 $file = Get-Content -Path $fileInfo.FullName -Raw
